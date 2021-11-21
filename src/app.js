d9bb617b79feb76bbdc3dbb8c6f387e69fabfba8
PjZito <94577616+PjZito@users.noreply.github.com>
2021-11-21 18:42:15 -0500

    added axios

---
+++ src/app.js
@@ -0,0 +1,4 @@
+let apiKey = "d89e4ba68ac7c22ff398fa2b3803df96";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=d89e4ba68ac7c22ff398fa2b3803df96`;
+
+console.log(apiUrl);
