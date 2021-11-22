8acf86bbb50e891f82ee9932a1e53475a8b74a77
PjZito <94577616+PjZito@users.noreply.github.com>
2021-11-21 19:42:52 -0500

    Showing the current Temperature

--- src/app.js
+++ src/app.js
@@ -1,4 +1,24 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
 let apiKey = "d89e4ba68ac7c22ff398fa2b3803df96";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=d89e4ba68ac7c22ff398fa2b3803df96`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&units=metric&appid=${apiKey}`;
 
 console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);
